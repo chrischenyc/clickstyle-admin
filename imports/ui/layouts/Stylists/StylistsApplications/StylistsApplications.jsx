324a5d4ba15ffdb4344e43bfbd12141f0f3bf76e
Chris Chen <chrischen79@gmail.com>
2017-11-03 20:32:00 +1100

    add filters to stylists applications page

--- imports/ui/layouts/Stylists/StylistsApplications/StylistsApplications.jsx
+++ imports/ui/layouts/Stylists/StylistsApplications/StylistsApplications.jsx
@@ -2,8 +2,22 @@ import React, { Component } from 'react';
 import StylistsApplicationsPage from './StylistsApplicationsPage';
 
 class StylistsApplications extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filter: 'pending',
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({ filter });
+  }
+
   render() {
-    return <StylistsApplicationsPage />;
+    return <StylistsApplicationsPage filter={this.state.filter} onFilter={this.handleFilter} />;
   }
 }
 
--- imports/ui/layouts/Stylists/StylistsApplications/StylistsApplicationsPage.jsx
+++ imports/ui/layouts/Stylists/StylistsApplications/StylistsApplicationsPage.jsx
@@ -1,5 +1,42 @@
 import React from 'react';
+import { Container, Header, Button } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
-const StylistsApplicationsPage = () => <div>to render</div>;
+const StylistsApplicationsPage = props => (
+  <Container>
+    <Header as="h2">Stylists Applications</Header>
+    <Button.Group>
+      <Button
+        active={props.filter === 'pending'}
+        onClick={() => {
+          props.onFilter('pending');
+        }}
+      >
+        Pending
+      </Button>
+      <Button
+        active={props.filter === 'approved'}
+        onClick={() => {
+          props.onFilter('approved');
+        }}
+      >
+        Approved
+      </Button>
+      <Button
+        active={props.filter === 'all'}
+        onClick={() => {
+          props.onFilter('all');
+        }}
+      >
+        Review
+      </Button>
+    </Button.Group>
+  </Container>
+);
+
+StylistsApplicationsPage.propTypes = {
+  filter: PropTypes.string.isRequired,
+  onFilter: PropTypes.func.isRequired,
+};
 
 export default StylistsApplicationsPage;
