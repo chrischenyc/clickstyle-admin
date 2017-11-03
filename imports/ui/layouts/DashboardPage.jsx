9263048ade4433a92d5ca8da897a0abd6090f453
Chris Chen <chrischen79@gmail.com>
2017-11-03 20:02:35 +1100

    add statistics placeholder to dashboard

--- imports/ui/layouts/DashboardPage.jsx
+++ imports/ui/layouts/DashboardPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Statistic } from 'semantic-ui-react';
 
 const DashboardPage = () => (
-  <Container style={{ padding: '4rem 0' }}>
-    <Header as="h2">Dashboard</Header>
-    <p>TODO: implement Dashboard</p>
+  <Container style={{ padding: '2rem 0' }}>
+    <Statistic.Group widths="three">
+      <Statistic>
+        <Statistic.Value>22,100</Statistic.Value>
+        <Statistic.Label>Users</Statistic.Label>
+      </Statistic>
+      <Statistic>
+        <Statistic.Value>4,200</Statistic.Value>
+        <Statistic.Label>Stylists</Statistic.Label>
+      </Statistic>
+      <Statistic>
+        <Statistic.Value>1,200</Statistic.Value>
+        <Statistic.Label>Bookings</Statistic.Label>
+      </Statistic>
+    </Statistic.Group>
   </Container>
 );
 
