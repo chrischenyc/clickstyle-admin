3a5b8b28d8992d5bb5789984e11243439b57a8ba
Chris Chen <chrischen79@gmail.com>
2017-12-25 12:45:26 +1100

    add sync shcema files script

---
+++ sync-schemas.sh
@@ -0,0 +1,7 @@
+rsync ../stylesquad-web/imports/api/addons/addons.js imports/api/addons/addons.js
+rsync ../stylesquad-web/imports/api/featured/featured.js imports/api/featured/featured.js
+rsync ../stylesquad-web/imports/api/products/products.js imports/api/products/products.js
+rsync ../stylesquad-web/imports/api/services/services.js imports/api/services/services.js
+rsync ../stylesquad-web/imports/api/stylist_applications/stylist_applications.js imports/api/stylist_applications/stylist_applications.js
+rsync ../stylesquad-web/imports/api/stylists/stylists.js imports/api/stylists/stylists.js
+rsync ../stylesquad-web/imports/api/suburbs/suburbs.js imports/api/suburbs/suburbs.js
