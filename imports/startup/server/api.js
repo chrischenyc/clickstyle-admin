f33481e169e9c1d728420f40c49712b9a5ff75be
Chris Chen <chrischen79@gmail.com>
2018-04-15 16:34:50 +1000

    update order

--- imports/startup/server/api.js
+++ imports/startup/server/api.js
@@ -1,26 +1,17 @@
-import '../../api/users/server/methods';
-import '../../api/users/server/publications';
-
-import '../../api/stylist_applications/server/methods';
-import '../../api/stylist_applications/server/publications';
-
-import '../../api/stylists/server/publications';
-import '../../api/stylists/server/methods';
-
-import '../../api/profiles/server/publications';
-
-import '../../api/services/server/publication';
-import '../../api/services/server/methods';
-
-import '../../api/addons/server/publications';
 import '../../api/addons/server/methods';
-
-import '../../api/suburbs/server/publications';
-import '../../api/suburbs/server/methods';
-
-import '../../api/featured/server/methods';
-
+import '../../api/addons/server/publications';
 import '../../api/bookings/server/methods';
 import '../../api/bookings/server/publications';
-
+import '../../api/featured/server/methods';
 import '../../api/notifications/server/methods';
+import '../../api/profiles/server/publications';
+import '../../api/services/server/publication';
+import '../../api/services/server/methods';
+import '../../api/stylist_applications/server/methods';
+import '../../api/stylist_applications/server/publications';
+import '../../api/stylists/server/publications';
+import '../../api/stylists/server/methods';
+import '../../api/suburbs/server/publications';
+import '../../api/suburbs/server/methods';
+import '../../api/users/server/methods';
+import '../../api/users/server/publications';
