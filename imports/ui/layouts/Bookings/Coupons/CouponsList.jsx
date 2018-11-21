973ceeff320c42158c0da94dee325fe6afe79856
Chris Chen <chrischen79@gmail.com>
2018-11-21 23:06:06 +1100

    Coupon: fix redeemed reporting

--- imports/ui/layouts/Bookings/Coupons/CouponsList.jsx
+++ imports/ui/layouts/Bookings/Coupons/CouponsList.jsx
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Table } from 'semantic-ui-react';
+import { Table, List } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
 
@@ -24,6 +24,7 @@ class CouponsList extends Component {
             <Table.HeaderCell>Code</Table.HeaderCell>
             <Table.HeaderCell>Discount</Table.HeaderCell>
             <Table.HeaderCell>Min Booking</Table.HeaderCell>
+            <Table.HeaderCell>Reusable</Table.HeaderCell>
             <Table.HeaderCell>Status</Table.HeaderCell>
             <Table.HeaderCell>Created</Table.HeaderCell>
             <Table.HeaderCell>Printed</Table.HeaderCell>
@@ -33,53 +34,62 @@ class CouponsList extends Component {
         </Table.Header>
 
         <Table.Body>
-          {this.props.ready &&
-            this.props.coupons.map(coupon => (
+          {this.props.ready
+            && this.props.coupons.map(coupon => (
               <Table.Row key={coupon._id}>
                 <Table.Cell>{coupon.code}</Table.Cell>
                 <Table.Cell>{coupon.discount}</Table.Cell>
                 <Table.Cell>{coupon.minBookingValue}</Table.Cell>
+                <Table.Cell>
+                  {coupon.reusable
+                    ? coupon.maxRedeems
+                      ? `${coupon.maxRedeems} redeems`
+                      : 'unlimited redeems'
+                    : 'no'}
+                </Table.Cell>
                 <Table.Cell>{coupon.status}</Table.Cell>
                 <Table.Cell>
-                  by <Link to={`/users/${coupon.createdBy}`}>{coupon.createdBy}</Link>
+                  by
+                  {' '}
+                  <Link to={`/users/${coupon.createdBy}`}>{coupon.createdBy}</Link>
                   <br />
                   {dateString(coupon.createdAt)}
                 </Table.Cell>
                 <Table.Cell>
-                  {coupon.printedBy &&
-                    coupon.printedAt && (
-                      <Fragment>
-                        by <Link to={`/users/${coupon.printedBy}`}>{coupon.printedBy}</Link>
-                        <br />
-                        {dateString(coupon.printedAt)}
-                      </Fragment>
-                    )}
+                  {coupon.printedBy && coupon.printedAt && (
+                    <Fragment>
+                      by
+                      {' '}
+                      <Link to={`/users/${coupon.printedBy}`}>{coupon.printedBy}</Link>
+                      <br />
+                      {dateString(coupon.printedAt)}
+                    </Fragment>
+                  )}
                 </Table.Cell>
 
                 <Table.Cell>
-                  {coupon.redeemedAt &&
-                    coupon.redeemedBooking && (
-                      <Fragment>
-                        booking:
-                        <Link to={`/bookings/${coupon.redeemedBooking}`}>
-                          {coupon.redeemedBooking}
-                        </Link>
-                        <br />
-                        {dateString(coupon.redeemedAt)}
-                      </Fragment>
-                    )}
+                  <List>
+                    {coupon.redeemedBookings
+                      && coupon.redeemedBookings.map(booking => (
+                        <List.Item key={booking.bookingId}>{dateString(booking.date)}</List.Item>
+                      ))}
+                  </List>
                 </Table.Cell>
 
                 <Table.Cell>
                   {coupon.expiry ? `expires by: ${dateString(coupon.expiry)}` : 'never expires'}
                   <br />
-                  {coupon.expiredBy &&
-                    coupon.expiredAt && (
-                      <Fragment>
-                        expired by {coupon.expiredBy}
-                        <br />on {dateString(coupon.expiredAt)}
-                      </Fragment>
-                    )}
+                  {coupon.expiredBy && coupon.expiredAt && (
+                    <Fragment>
+                      expired by
+                      {' '}
+                      {coupon.expiredBy}
+                      <br />
+                      on
+                      {' '}
+                      {dateString(coupon.expiredAt)}
+                    </Fragment>
+                  )}
                 </Table.Cell>
               </Table.Row>
             ))}
